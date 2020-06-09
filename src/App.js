92b943e838f296a0881812bc0b22ea597bfb30d4
gio <djgioandres@gmail.com>
2020-06-09 03:08:36 -0400

    Hello this is a test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hell this is a test.
+          Hello this is a test.
         </p>
         <a
           className="App-link"
